fdec6ed6cf9fc5b7da26900a9035db19449b5d9c
bhumesh <bhumesh117@gmail.com>
2021-05-22 20:18:28 +0530

    Initial commit

---
+++ dell_assignment/lib/model/carasoul_model.dart
@@ -0,0 +1,76 @@
+class CarasoulModel {
+  List<Items> items;
+
+  CarasoulModel({this.items});
+
+  CarasoulModel.fromJson(Map<String, dynamic> json) {
+    if (json['items'] != null) {
+      items = [];
+      json['items'].forEach((v) {
+        items.add(new Items.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.items != null) {
+      data['items'] = this.items.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Items {
+  String url;
+  String header;
+  String description;
+  List<Details> details;
+
+  Items({this.url, this.header, this.description, this.details});
+
+  Items.fromJson(Map<String, dynamic> json) {
+    url = json['url'];
+    header = json['header'];
+    description = json['description'];
+    if (json['details'] != null) {
+      details = new List<Details>();
+      json['details'].forEach((v) {
+        details.add(new Details.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['url'] = this.url;
+    data['header'] = this.header;
+    data['description'] = this.description;
+    if (this.details != null) {
+      data['details'] = this.details.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Details {
+  String url;
+  List<String> notes;
+  String id;
+
+  Details({this.url, this.notes, this.id});
+
+  Details.fromJson(Map<String, dynamic> json) {
+    url = json['url'];
+    notes = json['notes'].cast<String>();
+    id = json['id'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['url'] = this.url;
+    data['notes'] = this.notes;
+    data['id'] = this.id;
+    return data;
+  }
+}
